799a4c26776fcf0074c1da41b9f17105f1f2f5f0
Irina Bey <irinabey88@gmail.com>
2018-05-04 12:56:13 +0300

    + Task6 coderefactoring

--- NET.S.2018.LastName.Test/NET.S.2018.LastName.Test/Task6.Tests/CustomEnumerableTests.cs
+++ NET.S.2018.LastName.Test/NET.S.2018.LastName.Test/Task6.Tests/CustomEnumerableTests.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using Test6.Solution;
 
 namespace Task6.Tests
 {
@@ -15,16 +16,18 @@ namespace Task6.Tests
         public void Generator_ForSequence1()
         {
             int[] expected = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
+            NumberGenerator<int> number = new NumberGenerator<int>();
 
-            Assert.Inconclusive();
+            CollectionAssert.AreEqual(number.Generate(10, 1, 1, (x, y) => x + y), expected);
         }
 
         [Test]
         public void Generator_ForSequence2()
         {
             int[] expected = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512 };
+            NumberGenerator<int> number = new NumberGenerator<int>();
 
-            Assert.Inconclusive();
+            CollectionAssert.AreEqual(number.Generate(10, 2, 1, (x,y) => 6*x - 8*y), expected);
         }
 
         [Test]
@@ -32,7 +35,9 @@ namespace Task6.Tests
         {
             double[] expected = { 1, 2, 2.5, 3.3, 4.05757575757576, 4.87086926018965, 5.70389834408211, 6.55785277425587, 7.42763417076325, 8.31053343902137 };
 
-            Assert.Inconclusive();
+            NumberGenerator<double> number = new NumberGenerator<double>();
+
+            CollectionAssert.AreEqual(number.Generate(10, 2, 1, (x, y) => y - x/y), expected);
         }
     }
 }
---
+++ NET.S.2018.LastName.Test/NET.S.2018.LastName.Test/Test6.Solution/NumberGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Test6.Solution
+{
+    public class NumberGenerator<T> where T : struct
+    {
+        public IEnumerable<T> Generate(int count, T elementN, T elementNm1, Func<T,T,T> calNextElement) 
+        {
+            if (calNextElement == null)
+            {
+                throw new ArgumentNullException(nameof(calNextElement));
+            }
+
+            if (count < 1)
+            {
+                throw new ArgumentException($"Count numbers should be greater than 0!");
+            }
+            
+            yield return elementNm1;
+            yield return elementN;
+
+            T next = elementN;
+            T previous = elementNm1;
+
+            for (int i = 0; i < count - 2; i++)
+            {
+                T nextElement = calNextElement(next, previous);
+                previous = next;
+                next = nextElement;
+
+                yield return nextElement;
+            }
+        }
+    }
+}
